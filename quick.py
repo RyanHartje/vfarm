47888b1c6a10559692f982667ef891747c4f7294
ryanhartje <ryan@ryanhartje.com>
2016-03-25 20:19:42 -0500

    made quick a little shorter to be included in bash profile

--- quick.py
+++ quick.py
@@ -3,17 +3,6 @@ import serial
 import datetime
 
 serial_port = '/dev/ttyAMA0'
-ser = serial.Serial(serial_port, 9600)
-# turn on the LEDs
-ser.write("L,1\r")
-ser.write("C,1\r")
-line = ""
+ser = serial.Serial(serial_port, 9600, timeout=1)
 
-while True:
-  data = ser.read()
-  if(data == "\r"):
-    print "Received from sensor:" + line
-    line = ""
-  else:
-    line = line + data
-
+print("\nCurrent pH: %s\n" % data = ser.read())
